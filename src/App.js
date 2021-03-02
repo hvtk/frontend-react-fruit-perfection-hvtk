1aafc9c51a7724668290abdb4396e83d1c02c902
hvtk <hvtk1968@gmail.com>
2021-03-02 11:00:25 +0100

    import images

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
+import Product from "./Product";
 import React from 'react';
 import './App.css';
+
+import citroenen from './assets/citroenen.jpeg';
+import limoenen from './assets/limoenen.png';
+import ijsblokjes from './assets/ijsblokjes.jpg';
 
 function App() {
   return (
      <main>
-        <article className={"product"}>
-            <img src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg" alt="Citroen" />
-            <h2 className={"product-name"}> Citroen </h2>
-            <p className={"product-description"}> Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten.
+        <Product
+            image={citroenen}
+            title="Citroen"
+            description="Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten.
                 Van citroen kun je het vruchtvlees, het sap en de schil gebruiken.
-                Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel. </p>
-        </article>
+                Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
+        />
+
+         <Product
+             image={limoenen}
+             title="limoen"
+             description="Limoen is familie van de citroen en de sinaasappel en behoort tot de
+                citrusvruchten (Wijnruitfamilie). Limoenen zijn rond en kleiner dan citroenen.
+                De schil is dun, vrij glad en groen."
+         />
+
+         <Product
+             image={ijsblokjes}
+             title="ijsblokjes"
+             description="Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje.
+             Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen."
+         />
      </main>
   );
 }
+
 
 export default App;
